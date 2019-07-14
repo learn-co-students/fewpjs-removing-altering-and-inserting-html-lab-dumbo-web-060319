100e790b6d2349c818c4cbf016fb0b0101c689d6
Kevin Lai <kevinlai76@gmail.com>
2019-07-14 17:21:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("main").remove()
+
+let newHeader = document.createElement('h1');
+let newHeaderWithAttribute = newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Kevin is the champion'
