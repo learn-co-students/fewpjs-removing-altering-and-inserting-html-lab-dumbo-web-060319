0311e0e020ca8d512ec686b831f3b603bc9453ff
Richard <richardli125@gmail.com>
2019-07-14 12:13:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Select and Remove main element with the class main
+document.querySelector('main#main').remove()
+
+// Create a h1 element
+const newHeader = document.createElement('h1');
+
+// Add/Change an id to the h1 element
+newHeader.id = 'victory'
+
+// Add/Change the h1 innerHTML
+newHeader.innerHTML = 'Richard is the champion'
