1f1339ba028a66032b186aa608222cfd53958c7c
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-07-14 11:58:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+document.body.remove('#main')
+
+var newHeader = document.createElement('h1')
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = 'Abril is the champion';
+
+
+
+
